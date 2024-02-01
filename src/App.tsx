74ec5805baf0fa233369caa0ff81ff9ff9f92bdd
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2024-02-01 16:55:08 +0630

    feat: practice props states and children props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
-function App () {
-  return <div><ListGroup/></div>
+function App() {
+  const items = ["New York", "Los Angeles", "Chicago", "Houston", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
+    </div>
+  );
 }
 
 export default App;
