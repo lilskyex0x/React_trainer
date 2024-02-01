98c68cce1ee65a0aad03299fa86ba17a111776fc
Aung Pyae Khant <108471064+lilskyex0x@users.noreply.github.com>
2024-02-01 15:49:58 +0630

    feat: create test component

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'Lilsnow';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    else
+        return <h1>Hello Stranger</h1>;
+}
+
+export default Message;
